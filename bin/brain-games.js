806fe41b565c4bbfc96152c6656644dec3397018
lexey <likovalex@yandex.ru>
2024-10-12 18:57:11 +0300

    afterLinting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-console.log("node bin/brain-games.js\nWelcome to the Brain Games!");
-import { greeting } from '/home/lexey/frontend-project-44/src/cli.js'
+import greeting from '../src/cli.js';
+
+console.log('node bin/brain-games.js\nWelcome to the Brain Games!');
 greeting();
